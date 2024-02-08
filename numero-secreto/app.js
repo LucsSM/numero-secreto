1b449903142ccbc08c657f022881731522c16d51
LucasSM <lucas.silvamarra@gmail.com>
2024-02-08 19:08:52 -0300

    funcao reiniciarJogo e QoL do jogo

--- numero-secreto/app.js
+++ numero-secreto/app.js
@@ -1,36 +1,39 @@
 let listaDeNumerosAleatorios = [];
-let numeroAleatorio = gerarNumeroAleatorio();
+let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 let campoTitulo = document.querySelector("h1");
 let campoInstrucao = document.querySelector(".texto__paragrafo");
 let campoChute = document.querySelector(".container__input");
+let botaoReiniciar = document.getElementById("reiniciar");
 
-textoInicial();
-
-console.log(numeroAleatorio);
+console.log(numeroSecreto);
 console.log(listaDeNumerosAleatorios);
+
+exibirTextoInicial();
 
 function mudarTexto(campo, texto) {
     campo.innerText = texto;
 };
 
-function textoInicial() {
+function focarInput () {
+    campoChute.focus();
+}
+
+function exibirTextoInicial() {
     mudarTexto(campoTitulo, "Jogo do Numero Secreto");
     mudarTexto(campoInstrucao, "Informe um número de 1 a 10");
 }
 
 function gerarNumeroAleatorio() {
     let numeroAleatorio =  parseInt((Math.random() * 10) + 1);
+    let quantidadeDeNumerosNaLista = listaDeNumerosAleatorios.length;
 
-    if (listaDeNumerosAleatorios.includes(numeroAleatorio)) {
-        return gerarNumeroAleatorio();
-    } else if (listaDeNumerosAleatorios.length == 10) {
+    if (quantidadeDeNumerosNaLista == 10) {
         listaDeNumerosAleatorios = [];
-        listaDeNumerosAleatorios.push(numeroAleatorio);
-    } else {
-        listaDeNumerosAleatorios.push(numeroAleatorio);
     }
+
+    listaDeNumerosAleatorios.includes(numeroAleatorio) ? gerarNumeroAleatorio() : listaDeNumerosAleatorios.push(numeroAleatorio);
     
     return numeroAleatorio;
 }
@@ -38,17 +41,32 @@ function gerarNumeroAleatorio() {
 function chutar() {
     
     let textoTentativa = tentativas == 1 ? "tentativa" : "tentativas";
+    botaoReiniciar.removeAttribute("disabled");
+    focarInput();
 
-    if (campoChute.value > numeroAleatorio) {
+    if (campoChute.value > numeroSecreto) {
         mudarTexto(campoTitulo, "Errou!");
         mudarTexto(campoInstrucao, "O numero secreto é menor...");
         tentativas++;
-    } else if (campoChute.value < numeroAleatorio) {
+    } else if (campoChute.value < numeroSecreto) {
         mudarTexto(campoTitulo, "Errou!");
         mudarTexto(campoInstrucao, "O numero secreto é maior...");
         tentativas++;
     } else {
         mudarTexto(campoTitulo, "Acertou!");
         mudarTexto(campoInstrucao, ` Você acertou com ${tentativas} ${textoTentativa}`);
+        botaoReiniciar.focus();
     }
+}
+
+function reiniciarJogo() {
+    campoChute.value = "";
+    focarInput();
+    tentativas = 1;
+    exibirTextoInicial();
+    numeroSecreto = gerarNumeroAleatorio();
+    botaoReiniciar.setAttribute("disabled", true);
+
+    console.log(numeroSecreto);
+    console.log(listaDeNumerosAleatorios);
 }
