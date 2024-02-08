d35e29c9388a7975afe244b50841783ea413e2c8
LucasSM <lucas.silvamarra@gmail.com>
2024-02-08 14:24:14 -0300

    funcao de gerar numero aleatorio criada

--- numero-secreto/app.js
+++ numero-secreto/app.js
@@ -1,5 +1,12 @@
+let listaDeNumerosAleatorios = [];
 let campoTitulo = document.querySelector("h1");
 let campoInstrucao = document.querySelector(".texto__paragrafo");
+let numeroAleatorio = gerarNumeroAleatorio();
+
+textoInicial();
+
+console.log(numeroAleatorio);
+console.log(listaDeNumerosAleatorios);
 
 function mudarTexto(campo, texto) {
     campo.innerText = texto;
@@ -10,4 +17,17 @@ function textoInicial() {
     mudarTexto(campoInstrucao, "Informe um número de 1 a 10");
 }
 
-textoInicial();
+function gerarNumeroAleatorio() {
+    let numeroAleatorio =  parseInt((Math.random() * 10) + 1);
+
+    if (listaDeNumerosAleatorios.includes(numeroAleatorio)) {
+        return gerarNumeroAleatorio();
+    } else if (listaDeNumerosAleatorios.length == 10) {
+        listaDeNumerosAleatorios = [];
+        listaDeNumerosAleatorios.push(numeroAleatorio);
+    } else {
+        listaDeNumerosAleatorios.push(numeroAleatorio);
+    }
+    
+    return numeroAleatorio;
+}
